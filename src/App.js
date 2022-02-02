8a25803af473ad17574f969086984fb579c67b0d
RodolpheBeloncle <rbeloncle@gmail.com>
2022-02-02 19:05:25 +0100

    set router and fix maping wines

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<CellarPage />} />
-        <Route path="/edit-Selected-Wine/:id" element={<Wine />} />
+        <Route index element={<CellarPage />} />
+        <Route path="/editSelectedWine/:id" element={<Wine />} />
       </Routes>
     </div>
   );
